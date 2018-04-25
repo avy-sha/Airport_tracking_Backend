592d559110a0d35aa714723a5f3c327cecdf67db
Abhinav <abhinav2061997@gmail.com>
2018-04-25 21:07:25 +0530

    after t1

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ app.set('port', (80));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined', {stream: accessLogStream}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
@@ -46,28 +46,41 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
 });
 
-
 var pool = mysql.createPool({
     host: config.db.host,
     user: config.db.user,
     password: config.db.password,
     database: config.db.database
 });
-/*
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});*/
 
-app.get("/",function (req,res) {
+app.get("/", function (req, res) {
         return res.status(200).json("working");
     }
 );
 
 //get list of all the airports
-app.get("/airports/getall",function (req,res) {
-    pool.getConnection(function(err, con) {
-        con.query("SELECT * from Airport_Details", function (error, results, fields) {
+app.get("/airports/getall", function (req, res) {
+        pool.getConnection(function (err, con) {
+            con.query("SELECT * from Airport_Details", function (error, results, fields) {
+                if (error) {
+                    console.log(error);
+                    return res.status(400).json({"error": error});
+                }
+                else {
+                    con.release();
+                    return res.status(200).json(results);
+                }
+            });
+        })
+    }
+);
+
+//get list of all the devices installed at the airport
+app.post("/devices/getall", function (req, res) {
+    var Airport_id = req.body.Airport_id;
+    if (!Airport_id) return res.status(400).json({"error": "Airport_id required"});
+    pool.getConnection(function (err, con) {
+        con.query("SELECT * from Devices where Airport_id = ? and Node_id IS NOT NULL", [Airport_id], function (error, results, fields) {
             if (error) {
                 console.log(error);
                 return res.status(400).json({"error": error});
@@ -78,28 +91,10 @@ app.get("/airports/getall",function (req,res) {
             }
         });
     })
-}
-);
-
-//get list of all the devices installed at the airport
-app.post("/devices/getall",function(req,res){
-    var Airport_id = req.body.Airport_id;
-    if(!Airport_id)return res.status(400).json({"error":"Airport_id required"});
-    pool.getConnection(function(err, con) {
-        con.query("SELECT * from Devices where Airport_id = ? and Node_id IS NOT NULL", [Airport_id], function (error, results, fields) {
-            if (error) {
-                console.log(error);
-                return res.status(400).json({"error": error});
-            }
-            else { con.release();
-                return res.status(200).json(results);
-            }
-        });
-    })
 });
 
 //post the route in the table routes
-app.post("/routes/saveroute",function(req,res) {
+app.post("/routes/saveroute", function (req, res) {
     var Airport_id = req.body.Airport_id;
     var Route = req.body.Route;
 
@@ -114,7 +109,8 @@ app.post("/routes/saveroute",function(req,res) {
                 var Route_id = Airport_id + "_" + results[0]['count(*)'];
                 con.query("INSERT INTO Routes values(?,?,?)", [Route_id, Airport_id, Route], function (error, results, fields) {
                     if (error) throw err;
-                    else {con.release();
+                    else {
+                        con.release();
                         return res.status(200).json({"Message": "Route successfully created"});
                     }
                 });
@@ -124,22 +120,112 @@ app.post("/routes/saveroute",function(req,res) {
 })
 
 //Delete all entries for a particular airport from routes testing purpose only
-app.post("/routes/deleteall",function(req,res){
+app.post("/routes/deleteall", function (req, res) {
     var Airport_id = req.body.Airport_id;
-    pool.getConnection(function(err, con) {
+    pool.getConnection(function (err, con) {
         con.query("Delete from Routes where Route_id like ?; ", [Airport_id + '%'], function (error, results, fields) {
             if (error) {
                 console.log(error);
                 return res.status(400).json({"error": error});
             }
-            else {con.release();
+            else {
+                con.release();
                 return res.status(200).json({"message": "Delete Successful"});
             }
         });
     })
 });
+
+//add network to an airport
+app.post("/devices/addnetwork", function (req, res) {
+    var Airport_id = req.body.Airport_id;
+    if (!Airport_id) return res.status(400).json({"error": "Airport_id required"});
+    var Location = req.body.Location;
+    if (!Location) return res.status(400).json({"error": "Location required"});
+    pool.getConnection(function (err, con) {
+        con.query("select * from Devices where Airport_id = ? order by Devices.Device_id desc limit 1", [Airport_id], function (error, results, fields) {
+            if (error) {
+                console.log(error);
+                return res.status(400).json({"error": error});
+            }
+            else {
+                var a = JSON.stringify(results),
+                    b = JSON.parse(a),
+                    c = b[0].Device_id.split("_"),
+                    d = parseInt(c[1]) + 1,
+                    did = c[0] + "_" + d + "_",
+                    n = b[0].Network_id,
+                    nid = parseInt(n) + 1;
+                con.query("insert into Devices values(?, ?, ?, ?, ?, ?)", [did, Airport_id, nid, null, Location, 0], function (error, results, fields) {
+                    if (error) throw err;
+                    else {
+                        con.release();
+                        return res.status(200).json({"Message": "Row Added"});
+                    }
+                });
+            }
+        });
+    });
+
+});
+
+//add node to a specific network
+app.post("/devices/addnode", function (req, res) {
+    var Airport_id = req.body.Airport_id;
+    if (!Airport_id) return res.status(400).json({"error": "Airport_id required"});
+    var Device_id = req.body.Device_id;
+    if (!Device_id) return res.status(400).json({"error": "Device_id required"});
+    var Location = req.body.Location;
+    if (!Location) return res.status(400).json({"error": "Location required"});
+    pool.getConnection(function (err, con) {
+        con.query("select * from Devices where Airport_id = ? and Device_id = ? order by Device_id desc limit 1", [Airport_id, Device_id], function (error, results, fields) {
+            if (error) {
+                console.log(error);
+                return res.status(400).json({"error": error});
+            }
+            else {
+                var a = JSON.stringify(results),
+                    b = JSON.parse(a);
+                var c, d, did, n, nid, nc;
+                if (parseInt(b[0].Node_count) == 0) {
+                    c = b[0].Device_id.split("_");
+                    d = parseInt(c[1]) + 1;
+                    did = c[0] + "_" + c[1] + "_" + "0";
+                    n = b[0].Network_id;
+                    nid = 0;
+                    nc = 1;
+                }
+                else {
+                    c = b[0].Device_id.split("_");
+                    d = parseInt(b[0].Node_count);
+                    did = c[0] + "_" + c[1] + "_" + d;
+                    n = b[0].Network_id;
+                    nid = d;
+                    nc = parseInt(b[0].Node_count) + 1;
+                }
+                con.query("update Devices set Node_count = ? where Device_id = ?", [nc, Device_id], function (error, results, fields) {
+                    if (error) {
+                        console.log(error);
+                        return res.status(400).json({"error": error});
+                    }
+                    else {
+                        con.query("insert into Devices values(?, ?, ?, ?, ?, ?)", [did, Airport_id, n, nid, Location, -1], function (error, results, fields) {
+                            if (error) console.log(error);
+                            else {
+                                con.release();
+                                return res.status(200).json({"Message": "Row Added"});
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
